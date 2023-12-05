70a8a30864726550ff19c9110220baf272a8e0b2
LinksAwakes <152255968+LinksAwakes@users.noreply.github.com>
2023-12-06 01:07:56 +0800

    加了一丢丢交互
    点击屏幕会切换背景色

--- javascript.js
+++ javascript.js
@@ -1,2 +1,12 @@
+var colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#33FFF5']; // 预定义的颜色数组
+var currentColorIndex = 0; // 当前颜色索引
+
+function changeBackgroundColor() {
+    // 设置背景颜色
+    document.body.style.backgroundColor = colors[currentColorIndex];
+    
+    // 更新颜色索引
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+}
 
 
